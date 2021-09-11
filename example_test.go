c23ca58583f1192c7c2cb4a508e8980d580facee
edwardsbean <edwardsbean@gmail.com>
2021-09-11 18:41:50 +0800

    修复package

--- example_test.go
+++ example_test.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/gorilla/websocket"
+	"github.com/edwardsbean/websocket"
 )
 
 var (
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/gorilla/websocket
+module github.com/edwardsbean/websocket
 
 go 1.12
